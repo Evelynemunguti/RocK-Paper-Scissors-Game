a7b5f36e942386ec3cfb54bff6017f5b0e38fc5e
Evelyne Munguti <evelynekyamwai@gmail.com>
2025-03-31 21:47:06 +0300

    Added the computer choice of the game using the function gteComputerChoice

--- Rock-Paper-Scissors.js
+++ Rock-Paper-Scissors.js
@@ -1,7 +1,13 @@
-console.log("Hello Eve");
+//We create a function that will randomly return either 'rock', 'paper', or 'scissors'.
+//
+console.log("Hello Eve,Lets play a game");
 
+//this are all the choices we have
+const options=["Rock","Paper","Scissors"];
+
+//this function will represent the computer choice
 function getComputerChoice(){
-    const choices = ['rock', 'paper', 'scissors'];
-    const randomNumber = Math.floor(Math.random() * 3);
-    return choices[randomNumber];
+    const choice = options[Math.floor(Math.random()*options.length)];
+    return choice;
 }
+getComputerChoice();
